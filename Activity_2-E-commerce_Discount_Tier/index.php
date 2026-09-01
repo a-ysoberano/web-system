3e799c1a18a9ab66db4d91e590034575bb2355e5
sclurge03 <a.ysoberano@gmail.com>
2026-09-01 12:49:03 +0800

    Update Activity 2 E-commerce Discount Tier

--- Activity_2-E-commerce_Discount_Tier/index.php
+++ Activity_2-E-commerce_Discount_Tier/index.php
@@ -3,18 +3,14 @@ $total = $_GET['total'];
 
 if ($total >= 200) {
     $discountPrice = $total * 0.20;
-    $discount = "20%";
 } else if ($total >= 100) {
     $discountPrice = $total * 0.15;
-    $discount = "15%";
 } else if ($total >= 50) {
     $discountPrice = $total * 0.10;
-    $discount = "10%";
 } else {
     $discountPrice = 0;
-    $discount = "No Discount";
 }
 
 $finalPrice = $total - $discountPrice;
 
-echo "Total Price: $total, Discount: $discount, Final Price: $finalPrice";
+echo "Total Price: $total, Discount Amount: $discountPrice, Final Price: $finalPrice";
